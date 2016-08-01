6b96712527a715a436123177c46123971543af19
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2016-08-01 16:54:04 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703010026) do
+ActiveRecord::Schema.define(version: 20160801075218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "areas", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string   "Test"
