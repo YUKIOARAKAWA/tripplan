31a3e58607cb3171330e99e288bd69415439331a
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2016-08-03 18:14:40 +0900

    dispay info comment want

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,7 +5,7 @@ class Place < ActiveRecord::Base
   belongs_to :user
   belongs_to :plan
   has_many :pins
-  
+
   accepts_nested_attributes_for :pins
 
   #　追加されたplaceをプランの最後の設定する
@@ -13,4 +13,17 @@ class Place < ActiveRecord::Base
     max_cnt = Plan.find(plan_id).places.count
     self.route = max_cnt + 1
   end
+
+  #　行きたい度に応じて、⭐️を表示させる。
+  #　とりあえず、場所にひもづくピンのなかで、一番目の情報を表示する。
+  def show_star
+    if self.pins[0].want == 0
+      "⭐️"
+    elsif self.pins[0].want == 1
+      "⭐️⭐️"
+    else
+      "⭐️⭐️⭐️"
+    end
+  end
+
 end
