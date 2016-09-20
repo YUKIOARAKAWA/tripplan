dc84cc6c85e557f038845f97f32923cf75e893a4
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2016-09-20 21:10:35 +0900

    filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_plan
+
   # オーバライド
   # ログイン後のリダイレクト先をマイページに変更
   def after_sign_in_path_for(resource)
     top_mypage_path(current_user)
   end
 
+  # 検索フォーム用
+  def set_plan
+    @plan = Plan.new
+  end
 end
