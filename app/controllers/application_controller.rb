c2902d7b67eadc2d28131a97d487d50ac6cbadc4
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2016-09-28 19:33:31 +0900

    プロフィール変更画面でnameの編集を可能に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   before_action :set_plan
@@ -15,4 +16,12 @@ class ApplicationController < ActionController::Base
   def set_plan
     @plan = Plan.new
   end
+
+  protected
+
+  #　デバイスでnameを更新できるようにする
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
 end
