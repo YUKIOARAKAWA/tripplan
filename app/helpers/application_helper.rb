5707a7b7c0c54361637d883da04028ee9e34b4da
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2016-09-29 17:13:40 +0900

    都道府県の写真

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   # ユーザのプロフィール写真を表示する
   def profile_img(user)
     #if user.image_url?
@@ -20,7 +21,7 @@ module ApplicationHelper
   # プランに対して、写真やメンバーを表示する
   def plan_image(plan)
     img_url = "https://graph.facebook.com/#{user.uid}/picture?width=320&height=320"
-    image_tag(img_url, alt: user.name, class: 'img-responsive js-replace-no-image img-responsive img-circle', width: '100',height: '100', margin: '1px')
+    image_tag(img_url, alt: user.name, class: 'img-responsive js-replace-no-image img-responsive img-circle', width: '200',height: '200', margin: '1px')
   end
 
   # ユーザのプロフィール写真を表示する(header用)
