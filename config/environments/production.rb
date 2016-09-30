93246c599d5080bbcd7a17bbcab3577af18182db
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2016-09-30 17:29:28 +0900

    本番環境のメールの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'triplaning.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:               'smtp.gmail.com',
+  port:      	           587,
+  domain:                'example.com',
+  user_name:             'arakawayukio62@gmail.com',
+  password:              'zpqzfhyacagoyhso',
+  authentication:        'plain',
+  enable_starttls_auto:  true
+}
+
 end
